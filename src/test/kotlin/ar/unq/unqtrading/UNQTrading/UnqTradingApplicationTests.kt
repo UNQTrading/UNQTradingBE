76496fa4a78217c1e71d2042d5c31328e1a9de20
fabianfrangella <fabianfrangella@gmail.com>
2020-05-20 21:13:37 -0300

    initial commit con estructura de packages y gradle

---
+++ src/test/kotlin/ar/unq/unqtrading/UNQTrading/UnqTradingApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.unq.unqtrading.UNQTrading
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnqTradingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
