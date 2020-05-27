971a15b80846fcfb6cb4fab6c296f956486bfa8f
fabianfrangella <fabianfrangella@gmail.com>
2020-05-27 20:25:33 -0300

    agregado swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("mysql:mysql-connector-java")
+	implementation("org.springdoc:springdoc-openapi-ui:1.3.9")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
