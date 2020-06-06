0947a368bb88c2e406cddaaf4a5348128c0fb0a9
fabianfrangella <fabianfrangella@gmail.com>
2020-06-05 22:33:10 -0300

    test de integracion

---
+++ src/test/kotlin/ar/unq/unqtrading/DataService.kt
@@ -0,0 +1,28 @@
+package ar.unq.unqtrading
+
+import ar.unq.unqtrading.entities.OrdenDeVenta
+import ar.unq.unqtrading.repositories.OrdenDeVentaRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class DataService {
+    @Autowired
+    lateinit var ordenDeVentaRepository: OrdenDeVentaRepository
+
+    fun crearDatos() {
+            var orden1 = OrdenDeVenta()
+            var orden2 = OrdenDeVenta()
+            orden1.nombreEmpresa = "UNQ"
+            orden2.nombreEmpresa = "UNQ"
+            orden1.cantidadDeAcciones = 100
+            orden2.cantidadDeAcciones = 200
+            orden1.precio = 1000
+            orden2.precio = 2000
+            orden1.fechaDeVencimiento = LocalDate.of(2020,10,20)
+            orden2.fechaDeVencimiento = LocalDate.of(2020,10,20)
+            ordenDeVentaRepository.save(orden1)
+            ordenDeVentaRepository.save(orden2)
+    }
+}
---
+++ src/test/kotlin/ar/unq/unqtrading/integration/OrdenDeVentaIntegrationTest.kt
@@ -0,0 +1,28 @@
+package ar.unq.unqtrading.integration
+
+import ar.unq.unqtrading.DataService
+import ar.unq.unqtrading.services.OrdenDeVentaService
+import ar.unq.unqtrading.services.interfaces.IOrdenDeVentaService
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrdenDeVentaIntegrationTest {
+    @Autowired lateinit var ordenDeVentaService: IOrdenDeVentaService
+    @Autowired lateinit var dataService: DataService
+    
+    @BeforeEach
+    fun init() {
+        dataService.crearDatos()
+    }
+
+    @Test
+    fun findAllTest() {
+        var ordenes = ordenDeVentaService.findAll()
+        assertEquals(2, ordenes.size)
+    }
+}
