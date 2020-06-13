6e3e8bcdd87cf499f7dc58c125b273bde76649d9
andeehr <andeehr@gmail.com>
2020-06-13 14:16:21 -0300

    Se agrega fecha de ultima compra

--- src/main/kotlin/ar/unq/unqtrading/entities/Accion.kt
+++ src/main/kotlin/ar/unq/unqtrading/entities/Accion.kt
@@ -1,6 +1,8 @@
 package ar.unq.unqtrading.entities
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import java.time.LocalDate
 import javax.persistence.*
 
 @Entity
@@ -12,11 +14,14 @@ class Accion(){
     lateinit var nombreEmpresa: String
     @ManyToOne(cascade = [CascadeType.ALL])
     lateinit var usuario: Usuario
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    lateinit var fechaUltimaCompra: LocalDate
 
-    constructor(cantidad: Int, nombreEmpresa: String, usuario: Usuario) : this() {
+    constructor(cantidad: Int, nombreEmpresa: String, usuario: Usuario, ultimaCompra: LocalDate) : this() {
         this.cantidad = cantidad
         this.nombreEmpresa = nombreEmpresa
         this.usuario = usuario
+        this.fechaUltimaCompra = ultimaCompra
     }
 
     override fun equals(other: Any?): Boolean {
