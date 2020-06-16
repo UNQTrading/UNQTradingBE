05a3ce86f31a800454fae2460b94b1641c98f2e1
fabianfrangella <fabianfrangella@gmail.com>
2020-06-16 03:09:22 -0300

    agregado id en empresa

--- src/main/kotlin/ar/unq/unqtrading/entities/Empresa.kt
+++ src/main/kotlin/ar/unq/unqtrading/entities/Empresa.kt
@@ -1,7 +1,13 @@
 package ar.unq.unqtrading.entities
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity
 class Empresa() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int = 0
 }
