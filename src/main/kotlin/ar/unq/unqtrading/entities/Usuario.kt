ca866267c792fc2c7ded018ecbc14ec86ff3e2e6
fabianfrangella <fabianfrangella@gmail.com>
2020-06-27 23:57:17 -0300

    fix json serialize

--- src/main/kotlin/ar/unq/unqtrading/entities/Usuario.kt
+++ src/main/kotlin/ar/unq/unqtrading/entities/Usuario.kt
@@ -1,21 +1,28 @@
 package ar.unq.unqtrading.entities
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import javax.persistence.*
-import kotlin.reflect.KClass
 
 @Entity
 @Inheritance (strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "Tipo", discriminatorType = DiscriminatorType.STRING)
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.EXISTING_PROPERTY,
+        property = "type")
 @JsonSubTypes(
         JsonSubTypes.Type(value = Persona::class, name = "Persona"),
         JsonSubTypes.Type(value = Empresa::class, name = "Empresa"))
-abstract class Usuario  {
+abstract class Usuario () {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     open var id: Int = 0
 
     @Column(nullable = false)
     open var saldo: Int = 0
+
+    @JsonIgnore
+    open var type: String = this.javaClass.simpleName
 }
