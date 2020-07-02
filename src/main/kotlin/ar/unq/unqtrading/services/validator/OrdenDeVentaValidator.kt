89b1038d1603c4dde186f943993a9ffc4a43a6a2
andeehr <andeehr@gmail.com>
2020-07-02 20:57:43 -0300

    Se valida que una orden de venta no este repetida

--- src/main/kotlin/ar/unq/unqtrading/services/validator/OrdenDeVentaValidator.kt
+++ src/main/kotlin/ar/unq/unqtrading/services/validator/OrdenDeVentaValidator.kt
@@ -1,12 +1,17 @@
 package ar.unq.unqtrading.services.validator
 
 import ar.unq.unqtrading.entities.OrdenDeVenta
+import ar.unq.unqtrading.repositories.OrdenDeVentaRepository
 import ar.unq.unqtrading.services.exceptions.OrdenDeVentaIncorrectaException
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.time.LocalDate
 
 @Component
 class OrdenDeVentaValidator {
+    @Autowired
+    lateinit var ordenDeVentaRepository: OrdenDeVentaRepository
+
     fun validate(ordenDeVenta: OrdenDeVenta) {
         if (ordenDeVenta.cantidadDeAcciones < 1) {
             throw OrdenDeVentaIncorrectaException("La cantidad de acciones debe ser mayor a 0")
@@ -16,6 +21,14 @@ class OrdenDeVentaValidator {
         }
         if (ordenDeVenta.fechaDeVencimiento.isBefore(LocalDate.now().plusDays(1))) {
             throw OrdenDeVentaIncorrectaException("La fecha debe ser posterior a ${LocalDate.now()}")
+        }
+        if (ordenDeVentaRepository.findAllByCreadorId(ordenDeVenta.creador.id).any {
+                    it.cantidadDeAcciones == ordenDeVenta.cantidadDeAcciones &&
+                    it.empresa == ordenDeVenta.empresa &&
+                    it.fechaDeVencimiento == ordenDeVenta.fechaDeVencimiento &&
+                    it.precio == ordenDeVenta.precio
+                }) {
+            throw OrdenDeVentaIncorrectaException("Ya existe una orden de venta con esos datos")
         }
     }
 }
