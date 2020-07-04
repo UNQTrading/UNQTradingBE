43d9fd43b818845ff82196459c66b2800ae9f289
ggoffredo <gaston.goffredo@gmail.com>
2020-07-04 00:46:11 -0300

    test para validar que solamente se pueda crear una odv por una persona con cantidad mayor a la disponible - no funciona

--- src/test/kotlin/ar/unq/unqtrading/DataService.kt
+++ src/test/kotlin/ar/unq/unqtrading/DataService.kt
@@ -1,11 +1,14 @@
 package ar.unq.unqtrading
 
 import ar.unq.unqtrading.dto.OrdenDeVentaDTO
+import ar.unq.unqtrading.entities.Accion
 import ar.unq.unqtrading.entities.Empresa
 import ar.unq.unqtrading.entities.Persona
+import ar.unq.unqtrading.repositories.AccionRepository
 import ar.unq.unqtrading.repositories.EmpresaRepository
 import ar.unq.unqtrading.repositories.OrdenDeVentaRepository
 import ar.unq.unqtrading.repositories.PersonaRepository
+import ar.unq.unqtrading.services.AccionService
 import ar.unq.unqtrading.services.PersonaService
 import ar.unq.unqtrading.services.interfaces.IEmpresaService
 import ar.unq.unqtrading.services.interfaces.IOrdenDeVentaService
@@ -27,12 +30,16 @@ class DataService {
     lateinit var personaService: PersonaService
     @Autowired
     lateinit var personaRepository: PersonaRepository
+    @Autowired
+    lateinit var accionRepository: AccionRepository
+
     fun crearDatos() {
         var orden1 = OrdenDeVentaDTO()
         var orden2 = OrdenDeVentaDTO()
         var empresa = Empresa()
         var coca = Empresa()
         var persona = Persona()
+        var accionCoca = Accion()
         coca.nombreEmpresa = "Coca-Cola"
         coca.password = "12345"
         coca.email = "coca@coca.com"
@@ -41,6 +48,8 @@ class DataService {
         empresa.password = "1234578"
         empresa.email = "unq@unq.com"
         empresa.cuit = 98765541111
+        empresaService.save(empresa)
+        empresaService.save(coca)
         persona.nombre = "Federico"
         persona.apellido = "Garetti"
         persona.username = "Fede"
@@ -48,9 +57,12 @@ class DataService {
         persona.email = "fede@fede.com"
         persona.dni = 12345678
         persona.cuil = 20123456787
-        personaService.save(persona)
-        empresaService.save(empresa)
-        empresaService.save(coca)
+        accionCoca.empresa = coca
+        accionCoca.cantidad = 1
+        accionCoca.fechaUltimaCompra = LocalDate.now().plusDays(5)
+        accionCoca.persona =persona
+        persona.acciones.add(accionCoca)
+        accionRepository.save(accionCoca)
         orden1.nombreEmpresa = "UNQ"
         orden2.nombreEmpresa = "UNQ"
         orden1.cantidadDeAcciones = 100
@@ -67,6 +79,10 @@ class DataService {
 
     fun getIdEmpresaByNombre(nombre : String): Int? {
         return empresaRepository.findByNombreEmpresa(nombre)?.id
+    }
+
+    fun getIdPersonaByUsername(username : String): Int? {
+        return personaRepository.findByUsername(username)?.id
     }
 
     fun eliminarDatos() {
