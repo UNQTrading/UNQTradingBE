3f9ec620f5f42387d941ea71f1e8bacca726b9a2
ggoffredo <gaston.goffredo@gmail.com>
2020-07-04 18:01:00 -0300

    Al venderse la totalidad de las acciones se elimina la odv y la accion desaparece de las del usuario

--- src/main/kotlin/ar/unq/unqtrading/services/PersonaService.kt
+++ src/main/kotlin/ar/unq/unqtrading/services/PersonaService.kt
@@ -1,6 +1,7 @@
 package ar.unq.unqtrading.services
 
 import ar.unq.unqtrading.entities.Accion
+import ar.unq.unqtrading.entities.OrdenDeVenta
 import ar.unq.unqtrading.entities.Persona
 import ar.unq.unqtrading.repositories.AccionRepository
 import ar.unq.unqtrading.repositories.OrdenDeVentaRepository
@@ -31,17 +32,26 @@ class PersonaService : IPersonaService {
         orden.creador.saldo += orden.precio
 
         var vendedor = personaRepository.findById(orden.creador.id)
-        if (vendedor.isPresent) {
-            var accionVendedor = vendedor.get().acciones.find { accion -> accion.empresa == orden.empresa}
-            if(accionVendedor != null) {
-                accionVendedor.cantidad -= orden.cantidadDeAcciones
-                personaRepository.save(vendedor.get())
-            }
-        }
+        if (vendedor.isPresent)
+            descontarAcciones(vendedor.get(), orden)
 
-        ordenDeVentaRepository.save(orden)
+        //ordenDeVentaRepository.save(orden)
         personaRepository.save(persona)
         return accion
+    }
+
+    private fun descontarAcciones(vendedor: Persona, orden: OrdenDeVenta) {
+        var accionVendedor = vendedor.acciones.find { accion -> accion.empresa == orden.empresa }
+        if (accionVendedor != null) {
+            if (accionVendedor.cantidad != orden.cantidadDeAcciones)
+                accionVendedor.cantidad -= orden.cantidadDeAcciones
+            else {
+                vendedor.acciones.remove(accionVendedor)
+                ordenDeVentaRepository.delete(orden)
+                accionRepository.delete(accionVendedor)
+            }
+            personaRepository.save(vendedor)
+        }
     }
 
     override fun findById(personaId: Int): Persona {
