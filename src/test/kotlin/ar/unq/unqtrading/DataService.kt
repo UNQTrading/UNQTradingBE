f7066877eaa417ae7d59245ffbf4371b95a5a9f0
ggoffredo <gaston.goffredo@gmail.com>
2020-07-04 00:50:47 -0300

    test para validar que solamente se pueda crear una odv por una persona con cantidad mayor a la disponible

--- src/test/kotlin/ar/unq/unqtrading/DataService.kt
+++ src/test/kotlin/ar/unq/unqtrading/DataService.kt
@@ -87,6 +87,7 @@ class DataService {
 
     fun eliminarDatos() {
         ordenDeVentaRepository.deleteAll()
+        accionRepository.deleteAll()
         empresaRepository.deleteAll()
         personaRepository.deleteAll()
     }
